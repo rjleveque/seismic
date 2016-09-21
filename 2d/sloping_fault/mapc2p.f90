e75508f906ad1d8ddf50099a8e97170b5446ca1a
cjvogl <cvogl@uw.edu>
2016-09-21 10:56:05 -0700

    had to manually merge changes to sloping_fault

--- 2d/sloping_fault/mapc2p.f90
+++ 2d/sloping_fault/mapc2p.f90
@@ -10,32 +10,55 @@
     real (kind=8), intent(out) :: xp,yp
 
     ! Variables from setprob:
-    real (kind=8) :: center(2), theta, xcb(2), mindepth
-    common /fault/  center, theta, xcb, mindepth
+    real (kind=8) :: fcenter(2), theta, xcb(2), mindepth
+    common /fault/  fcenter, theta, xcb, mindepth
+
+    real (kind=8) :: bacenter, bawidth, baelev, slcenter, slwidth, &
+      shecenter, shewidth, sheelev, becenter, bewidth, shoelev
+    common /surface/  bacenter, bawidth, baelev, slcenter, slwidth, &
+      shecenter, shewidth, sheelev, becenter, bewidth, shoelev
+
 
     ! Local variables
-    real (kind=8) :: ls, alpha, xrot, yrot
+    real (kind=8) :: ls, alpha, xrot, yrot, xsf, ysf, tol
 
-    if (xc < xcb(1)) then
-      ls = dsqrt((xc-xcb(1))**2 + (yc-center(2))**2)
-    elseif (xc > xcb(2)) then
-      ls = dsqrt((xc-xcb(2))**2 + (yc-center(2))**2)
+    tol = mindepth
+
+    ! Grid aligned with surface (xsf,ysf)
+    xsf = xc
+    if (xc < bacenter + 0.5d0*bawidth) then
+      ysf = yc + baelev
+    elseif (xc < slcenter + 0.5d0*slwidth) then
+      ysf = yc + baelev + ((xc - slcenter)/slwidth + 0.5d0)*(sheelev - baelev)
+    elseif (xc < shecenter + 0.5d0*shewidth) then
+      ysf = yc + sheelev
+    elseif (xc < becenter + 0.5d0*bewidth) then
+      ysf = yc + sheelev + ((xc - becenter)/bewidth + 0.5d0)*(shoelev - sheelev)
     else
-      ls = dabs(yc - center(2))
+      ysf = yc + shoelev
     end if
 
-    alpha = ls/mindepth
-    xrot = center(1) + dcos(theta)*(xc-center(1)) + dsin(theta)*(yc-center(2))
-    yrot = center(2) - dsin(theta)*(xc-center(1)) + dcos(theta)*(yc-center(2))
-    !yrot = yc - dsin(theta)*(xc-center(1))
-
-    if (alpha < 1.d0) then
-      xp = (1.d0-alpha)*xrot + alpha*xc
-      !xp = xc
-      yp = (1.d0-alpha)*yrot + alpha*yc
+    ! Grid aligned with fault (xrot,yrot)
+    if (xc < xcb(1)) then
+      ls = dsqrt((xc-xcb(1))**2 + (yc-fcenter(2))**2)
+    elseif (xc > xcb(2)) then
+      ls = dsqrt((xc-xcb(2))**2 + (yc-fcenter(2))**2)
     else
-      xp = xc
-      yp = yc
+      ls = dabs(yc - fcenter(2))
+    end if
+
+    alpha = ls/tol
+    xrot = fcenter(1) + dcos(theta)*(xc-fcenter(1)) + dsin(theta)*(yc-fcenter(2))
+    yrot = fcenter(2) - dsin(theta)*(xc-fcenter(1)) + dcos(theta)*(yc-fcenter(2))
+
+    ! Linear interpolation between two grids
+    if (alpha < 1.d0) then
+      xp = (1.d0-alpha)*xrot + alpha*xsf
+      !xp = xc
+      yp = (1.d0-alpha)*yrot + alpha*ysf
+    else
+      xp = xsf
+      yp = ysf
     end if
 
     return
--- 2d/sloping_fault/setplot_surface.py
+++ 2d/sloping_fault/setplot_surface.py
@@ -1,11 +1,11 @@
 
-""" 
+"""
 Set up the plot figures, axes, and items to be done for each frame.
 
 This module is imported by the plotting routines and then the
 function setplot is called to set the plot parameters.
-    
-""" 
+
+"""
 
 import numpy as np
 from mapc2p import mapc2p
@@ -28,9 +28,6 @@ xp2 = xcenter + 0.5*width*np.cos(theta)
 yp1 = ycenter + 0.5*width*np.sin(theta)
 yp2 = ycenter - 0.5*width*np.sin(theta)
 
-xlimits = [-0.5*probdata.domain_width,0.5*probdata.domain_width]
-ylimits = [-probdata.domain_depth,0.0]
-
 gdata = np.loadtxt('gauges.data',skiprows=7)
 ngauges = gdata.shape[0]
 print "Found %s gauges" % ngauges
@@ -40,13 +37,13 @@ yc = gdata[:,2]
 #--------------------------
 def setplot(plotdata):
 #--------------------------
-    
-    """ 
+
+    """
     Specify what is to be plotted at each frame.
     Input:  plotdata, an instance of clawpack.visclaw.data.ClawPlotData.
     Output: a modified version of plotdata.
-    
-    """ 
+
+    """
 
 
     from clawpack.visclaw import colormaps
@@ -62,7 +59,7 @@ def setplot(plotdata):
         xs = zeros(ngauges)
         ys = zeros(ngauges)
         for j in range(ngauges):
-            gaugeno = goffset + j 
+            gaugeno = goffset + j
             g = plotdata.getgauge(gaugeno)
             for k in range(1,len(g.t)):
                 if g.t[k] > t:
@@ -81,13 +78,20 @@ def setplot(plotdata):
         legend()
 
     plotdata.afterframe = afterframe
-    
-    def plot_interfaces(current_data):
-        from pylab import linspace, plot, sin, pi
+
+    def plot_fault(current_data):
+        from pylab import linspace, plot
         xl = linspace(xp1,xp2,100)
         yl = linspace(yp1,yp2,100)
         plot(xl,yl,'k')
-    
+
+    def plot_seafloor(current_data):
+        from pylab import linspace, plot
+        from mapc2p import topo
+        x = linspace(-100e3,1.0,1000)
+        y = topo(x)
+        plot(x,y,'k')
+
 
     def sigmatr(current_data):
         # return -trace(sigma)
@@ -109,17 +113,17 @@ def setplot(plotdata):
         ux = (u[I+1,:][:,J] - u[I-1,:][:,J]) / (2*dx)
         vy = (v[:,J+1][I,:] - v[:,J-1][I,:]) / (2*dy)
         dint = ux + vy
-        
+
         #zx = zeros((mx-2,1))
         #zy = zeros((1,my))
         #d = vstack((zy, hstack((zx, ux+vy, zx)), zy))
-        
+
         d0 = dint[:,0]
         d1 = dint[:,-1]
         d2 = vstack((d0, dint.T, d1)).T
         d0 = d2[0,:]
         d1 = d2[-1,:]
-        d = vstack((d0,d2,d1))      
+        d = vstack((d0,d2,d1))
         return d
 
     def curl(current_data):
@@ -143,7 +147,7 @@ def setplot(plotdata):
         c2 = vstack((c0, cint.T, c1)).T
         c0 = c2[0,:]
         c1 = c2[-1,:]
-        c = vstack((c0,c2,c1))      
+        c = vstack((c0,c2,c1))
 
         # to set curl to zero near patch edges...
         #c = zeros(u.shape)
@@ -151,161 +155,84 @@ def setplot(plotdata):
 
         return c
 
-    # Figure for trace(sigma) 
-    plotfigure = plotdata.new_plotfigure(name='trace', figno=10)
+    # Figure for seismic scale trace and surface deformation
+    plotfigure = plotdata.new_plotfigure(name='seismic_scale', figno=10)
     plotfigure.kwargs = {'figsize':(8,8)}
 
     # Set up for axes in this figure:
     plotaxes = plotfigure.new_plotaxes()
     plotaxes.axescmd = 'subplot(212)'
-    #plotaxes.xlimits = [-75e3, 125e3]
-    #plotaxes.ylimits = [-50e3,0]
-    plotaxes.xlimits = xlimits
-    plotaxes.ylimits = ylimits
+    plotaxes.xlimits = 'auto'
+    plotaxes.ylimits = 'auto'
     plotaxes.title = '-trace(sigma)'
     plotaxes.scaled = True
-    plotaxes.afteraxes = plot_interfaces
+    plotaxes.afteraxes = plot_fault
 
     # Set up for item on these axes:
     plotitem = plotaxes.new_plotitem(plot_type='2d_pcolor')
-    plotitem.plot_var = sigmatr
+    plotitem.plot_var = 4
     plotitem.pcolor_cmap = colormaps.blue_white_red
-    plotitem.pcolor_cmin = -2e6
-    plotitem.pcolor_cmax = 2e6
+    plotitem.pcolor_cmin = -0.1
+    plotitem.pcolor_cmax = 0.1
     plotitem.add_colorbar = False
     plotitem.amr_celledges_show = [0,0]
     plotitem.amr_patchedges_show = [0]
     plotitem.MappedGrid = True
     plotitem.mapc2p = mapc2p
 
-
-
-
-    # Figure for trace(sigma) and sigma_12 side by side
-    plotfigure = plotdata.new_plotfigure(name='P and S waves', figno=11)
-    plotfigure.show = False
-    plotfigure.kwargs = {'figsize':(12,12)}
+    # Figure for seismic scale trace and surface deformation
+    plotfigure = plotdata.new_plotfigure(name='tsunami_scale', figno=11)
+    plotfigure.kwargs = {'figsize':(8,8)}
 
     # Set up for axes in this figure:
     plotaxes = plotfigure.new_plotaxes()
-    plotaxes.axescmd = 'subplot(511)'
-    plotaxes.xlimits = 'auto'
-    plotaxes.ylimits = 'auto'
+    plotaxes.axescmd = 'subplot(211)'
+    plotaxes.xlimits = [-100e3,0]
+    plotaxes.ylimits = [-5e3,0]
     plotaxes.title = '-trace(sigma)'
-    plotaxes.scaled = True
-    #plotaxes.afteraxes = plot_interfaces
+    plotaxes.scaled = False
+    plotaxes.afteraxes = plot_seafloor
 
     # Set up for item on these axes:
     plotitem = plotaxes.new_plotitem(plot_type='2d_pcolor')
     plotitem.plot_var = sigmatr
     plotitem.pcolor_cmap = colormaps.blue_white_red
-    plotitem.pcolor_cmin = -0.03 * cscale
-    plotitem.pcolor_cmax = 0.03 * cscale
+    plotitem.pcolor_cmin = -2.0e6
+    plotitem.pcolor_cmax = 2.0e6
     plotitem.add_colorbar = False
-    plotitem.amr_celledges_show = [1,0]
+    plotitem.amr_celledges_show = [0,0]
     plotitem.amr_patchedges_show = [0]
     plotitem.MappedGrid = True
     plotitem.mapc2p = mapc2p
 
-
+    # Set up for axes in this figure:
     plotaxes = plotfigure.new_plotaxes()
-    plotaxes.axescmd = 'subplot(512)'
-    plotaxes.xlimits = 'auto'
-    plotaxes.ylimits = 'auto'
-    plotaxes.title = 'div(u)'
-    plotaxes.scaled = True
-    #plotaxes.afteraxes = plot_interfaces
-
-    # Set up for item on these axes:
-    plotitem = plotaxes.new_plotitem(plot_type='2d_pcolor')
-    plotitem.plot_var = div
-    plotitem.pcolor_cmap = colormaps.blue_white_red
-    plotitem.pcolor_cmin = -0.1 * cscale
-    plotitem.pcolor_cmax = 0.1 * cscale
-    plotitem.add_colorbar = False
-    plotitem.amr_celledges_show = [False]
-    plotitem.amr_patchedges_show = [0]
-    plotitem.MappedGrid = True
-    plotitem.mapc2p = mapc2p
-
-
-    # Figure for curl:
-    plotaxes = plotfigure.new_plotaxes()
-    plotaxes.axescmd = 'subplot(513)'
-    plotaxes.xlimits = 'auto'
-    plotaxes.ylimits = 'auto'
-    plotaxes.title = 'curl(u)'
-    plotaxes.scaled = True
-    #plotaxes.afteraxes = plot_interfaces
-
-    # Set up for item on these axes:
-    plotitem = plotaxes.new_plotitem(plot_type='2d_pcolor')
-    plotitem.plot_var = curl
-    plotitem.pcolor_cmap = colormaps.blue_white_red
-    plotitem.pcolor_cmin = -0.2 * cscale
-    plotitem.pcolor_cmax = 0.2 * cscale
-    plotitem.add_colorbar = False
-    plotitem.colorbar_shrink = 0.7
-    plotitem.amr_celledges_show = [False]
-    plotitem.amr_patchedges_show = [0]
-    plotitem.MappedGrid = True
-    plotitem.mapc2p = mapc2p
-
-
-    # Figure for x-velocity:
-    plotaxes = plotfigure.new_plotaxes()
-    plotaxes.axescmd = 'subplot(514)'
-    plotaxes.xlimits = 'auto'
-    plotaxes.ylimits = 'auto'
-    plotaxes.title = 'x-velocity'
-    plotaxes.scaled = True
-    #plotaxes.afteraxes = plot_interfaces
-
-    # Set up for item on these axes:
-    plotitem = plotaxes.new_plotitem(plot_type='2d_pcolor')
-    plotitem.plot_var = 3
-    plotitem.pcolor_cmap = colormaps.blue_white_red
-    plotitem.pcolor_cmin = -0.005 * cscale
-    plotitem.pcolor_cmax = 0.005 * cscale
-    plotitem.add_colorbar = False
-    plotitem.colorbar_shrink = 0.7
-    plotitem.amr_celledges_show = [False]
-    plotitem.amr_patchedges_show = [0]
-    plotitem.MappedGrid = True
-    plotitem.mapc2p = mapc2p
-
-
-    # Figure for y-velocity:
-    plotaxes = plotfigure.new_plotaxes()
-    plotaxes.axescmd = 'subplot(515)'
-    plotaxes.xlimits = 'auto'
-    plotaxes.ylimits = 'auto'
-    plotaxes.title = 'y-velocity'
-    plotaxes.scaled = True
-    #plotaxes.afteraxes = plot_interfaces
+    plotaxes.axescmd = 'subplot(212)'
+    plotaxes.xlimits = [-100e3,0]
+    plotaxes.ylimits = [-5e3,0]
+    plotaxes.title = 'y velocity'
+    plotaxes.scaled = False
+    plotaxes.afteraxes = plot_seafloor
 
     # Set up for item on these axes:
     plotitem = plotaxes.new_plotitem(plot_type='2d_pcolor')
     plotitem.plot_var = 4
     plotitem.pcolor_cmap = colormaps.blue_white_red
-    plotitem.pcolor_cmin = -0.005 * cscale
-    plotitem.pcolor_cmax = 0.005 * cscale
+    plotitem.pcolor_cmin = -0.1
+    plotitem.pcolor_cmax = 0.1
     plotitem.add_colorbar = False
-    plotitem.colorbar_shrink = 0.7
-    plotitem.amr_celledges_show = [False]
+    plotitem.amr_celledges_show = [0,0]
     plotitem.amr_patchedges_show = [0]
     plotitem.MappedGrid = True
     plotitem.mapc2p = mapc2p
-
-
 
     # Figure for grid cells
     plotfigure = plotdata.new_plotfigure(name='cells', figno=2)
 
     # Set up for axes in this figure:
     plotaxes = plotfigure.new_plotaxes()
-    plotaxes.xlimits = xlimits
-    plotaxes.ylimits = ylimits
+    plotaxes.xlimits = 'auto'
+    plotaxes.ylimits = 'auto'
     plotaxes.title = 'Level 4 grid patches'
     plotaxes.scaled = True
 
@@ -365,5 +292,3 @@ def setplot(plotdata):
     plotdata.parallel = True
 
     return plotdata
-
-    
